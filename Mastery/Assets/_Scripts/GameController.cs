014ca3ab72ecc773d4602c4f464b67d9fd49a772
hollowaytape <hollowaytape@retro-type.com>
2017-09-21 00:18:57 -0400

    Revert "Input and animations for light attacks; detect sword collisions"
    This reverts commit 9be736c7df78a2da1c3ea66b6b7d5a0514d8a128.

--- Mastery/Assets/_Scripts/GameController.cs
+++ Mastery/Assets/_Scripts/GameController.cs
@@ -10,17 +10,6 @@ public class GameController : MonoBehaviour {
     private void Update()
     {
         //Input reading goes here
-        if (Input.GetButtonDown("Fire1"))
-        {
-            playerOne.state = PlayerController.CharacterState.ATTACKING;
-            playerOne.anim.Play("Light Attack");
-        }
-
-        if (Input.GetButtonDown("Fire2"))
-        {
-            playerTwo.state = PlayerController.CharacterState.ATTACKING;
-            playerTwo.anim.Play("Light Attack");
-        }
     }
 
     private void FixedUpdate()
