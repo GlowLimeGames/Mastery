80835c93e9b7e2ec77f9823dc58383376d1c3b7d
wsattanuparp <willsattanuparp@gmail.com>
2017-09-27 17:23:14 -0400

    Added start and end screen, implemented stock system

---
+++ Mastery/Assets/_Scripts/StockLives.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StockLives : MonoBehaviour {
+
+    //create an array of gameobjects
+    //make them disappear on event
+    public GameObject[] stocks;
+    private int size;
+	// Use this for initialization
+	void Start () {
+        size = stocks.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnEnable()
+    {
+        StockManager.onFatal += RemoveStock;
+    }
+    private void OnDisable()
+    {
+        StockManager.onFatal -= RemoveStock;
+    }
+    void RemoveStock() {
+        if (size > 1)
+        {
+            stocks[size - 1].SetActive(false);
+            GameObject[] temp = new GameObject[size-1];
+            for (int i = 0; i < size - 1; i++) {
+                temp[i] = stocks[i];
+            }
+            stocks = temp;
+            size--;
+        }
+        else {
+            //game over
+            Debug.Log("game ended");
+            SceneManager.LoadScene(2);
+        }
+    }
+}
---
+++ Mastery/Assets/_Scripts/StockManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockManager : MonoBehaviour {
+
+    public delegate void FatalEvent();
+    public static event FatalEvent onFatal;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Click")){
+            if (onFatal != null) {
+                onFatal();
+            }
+        }
+    }
+}
