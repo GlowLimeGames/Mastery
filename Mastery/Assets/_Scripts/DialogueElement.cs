656408d8702d9b78d2746d8e27d25d27f31f3a79
wsattanuparp <willsattanuparp@gmail.com>
2017-11-01 17:38:49 -0400

    Implemented a dialogue box

---
+++ Mastery/Assets/_Scripts/DialogueElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueElement : MonoBehaviour {
+
+    public FullConversation conversation;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(conversation);
+
+    }
+}
---
+++ Mastery/Assets/_Scripts/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<Dialogue> sentences;
+    void Start()
+    {
+        sentences = new Queue<Dialogue>();
+    }
+    public void StartDialogue(FullConversation conversation)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = conversation.dialogue[0].nameOfSpeaker;
+        sentences.Clear();
+
+        foreach (Dialogue sentence in conversation.dialogue) {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        Dialogue sentence = sentences.Dequeue();
+        nameText.text = sentence.nameOfSpeaker;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence.sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialogue() {
+        animator.SetBool("IsOpen", false);
+    }
+}
